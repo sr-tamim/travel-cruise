a4bd09c6e5803e255a73c08bb4dd684cf7a3b338
SR TAMIM <saifur.tamim@gmail.com>
2021-10-30 02:09:55 +0600

    navbar admin actions and firebase reload router error

--- src/components/Shared/Navbar/Navbar.js
+++ src/components/Shared/Navbar/Navbar.js
@@ -1,16 +1,18 @@
 import "./Navbar.css";
 import { Fragment, useContext } from 'react';
-import { Disclosure, Menu, Transition } from '@headlessui/react';
-import { MenuIcon, UserIcon, XIcon } from '@heroicons/react/outline';
+import { Disclosure, Menu, Popover, Transition } from '@headlessui/react';
+import { ChevronDownIcon, MenuIcon, UserIcon, XIcon } from '@heroicons/react/outline';
 import { UserContext } from '../../../contexts/UserContext';
 import { NavLink } from 'react-router-dom';
 import navBackChange from "../../../utilities/navBackChange";
 
 const navigation = [
-    { name: 'Home', href: '/home', current: false },
-    { name: 'Team', href: '/home', current: true },
-    { name: 'Projects', href: '/home', current: false },
-    { name: 'Contact', href: '/contact', current: false },
+    { name: 'Home', href: '/home' },
+    { name: 'Contact', href: '/contact' },
+]
+const adminActions = [
+    { name: 'Manage Orders', href: '/manage/orders' },
+    { name: 'Add Places', href: '/places/add' }
 ]
 
 function classNames(...classes) {
@@ -61,6 +63,55 @@ export default function Navbar() {
                                                 {item.name}
                                             </NavLink>
                                         ))}
+                                        <Popover className="relative">
+                                            {({ open }) => (
+                                                <>
+                                                    <Popover.Button
+                                                        className={classNames(
+                                                            open ? 'text-green-600 font-bold' : 'text-black',
+                                                            'group rounded-md inline-flex items-center text-base font-medium hover:bg-green-100 md:px-3 pt-1 z-50'
+                                                        )}
+                                                    >
+                                                        <span>Admin Actions</span>
+                                                        <ChevronDownIcon
+                                                            className={classNames(
+                                                                open ? 'text-gray-600' : 'text-gray-400',
+                                                                'ml-2 h-5 w-5 group-hover:text-gray-500'
+                                                            )}
+                                                            aria-hidden="true"
+                                                        />
+                                                    </Popover.Button>
+
+                                                    <Transition
+                                                        as={Fragment}
+                                                        enter="transition ease-out duration-200"
+                                                        enterFrom="opacity-0 translate-y-1"
+                                                        enterTo="opacity-100 translate-y-0"
+                                                        leave="transition ease-in duration-150"
+                                                        leaveFrom="opacity-100 translate-y-0"
+                                                        leaveTo="opacity-0 translate-y-1"
+                                                    >
+                                                        <Popover.Panel className="absolute z-50 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
+                                                            <div className="rounded-lg shadow-lg overflow-hidden">
+                                                                <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                                                                    {adminActions.map((item) => (
+                                                                        <a
+                                                                            key={item.name}
+                                                                            href={item.href}
+                                                                            className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
+                                                                        >
+                                                                            <div className="ml-4">
+                                                                                <p className="text-base font-medium text-gray-900">{item.name}</p>
+                                                                            </div>
+                                                                        </a>
+                                                                    ))}
+                                                                </div>
+                                                            </div>
+                                                        </Popover.Panel>
+                                                    </Transition>
+                                                </>
+                                            )}
+                                        </Popover>
                                     </div>
                                 </div>
                             </div>
@@ -131,20 +182,69 @@ export default function Navbar() {
                         </div>
                     </div>
 
-                    <Disclosure.Panel className="sm:hidden bg-white">
+                    <Disclosure.Panel className="sm:hidden bg-white py-3">
                         <div className="px-2 pt-2 pb-3 space-y-1">
                             {navigation.map((item) => (
                                 <Disclosure.Button
                                     key={item.name}
-                                    as="a"
-                                    href={item.href}
                                     className={classNames('text-black hover:bg-green-100 block px-3 py-2 rounded-md text-base'
                                     )}
                                     aria-current={item.current ? 'page' : undefined}
                                 >
-                                    {item.name}
+                                    <NavLink to={item.href}>
+                                        {item.name}
+                                    </NavLink>
                                 </Disclosure.Button>
                             ))}
+                            <Popover className="relative">
+                                {({ open }) => (
+                                    <>
+                                        <Popover.Button
+                                            className={classNames(
+                                                open ? 'text-green-600 font-bold' : 'text-black',
+                                                'group rounded-md inline-flex items-center text-base font-medium hover:bg-green-100 px-3 py-2 z-50'
+                                            )}
+                                        >
+                                            <span>Admin Actions</span>
+                                            <ChevronDownIcon
+                                                className={classNames(
+                                                    open ? 'text-gray-600' : 'text-gray-400',
+                                                    'ml-2 h-5 w-5 group-hover:text-gray-500'
+                                                )}
+                                                aria-hidden="true"
+                                            />
+                                        </Popover.Button>
+
+                                        <Transition
+                                            as={Fragment}
+                                            enter="transition ease-out duration-200"
+                                            enterFrom="opacity-0 translate-y-1"
+                                            enterTo="opacity-100 translate-y-0"
+                                            leave="transition ease-in duration-150"
+                                            leaveFrom="opacity-100 translate-y-0"
+                                            leaveTo="opacity-0 translate-y-1"
+                                        >
+                                            <Popover.Panel className="absolute z-50 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
+                                                <div className="rounded-lg shadow-lg overflow-hidden">
+                                                    <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                                                        {adminActions.map((item) => (
+                                                            <a
+                                                                key={item.name}
+                                                                href={item.href}
+                                                                className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
+                                                            >
+                                                                <div className="ml-4">
+                                                                    <p className="text-base font-medium text-gray-900">{item.name}</p>
+                                                                </div>
+                                                            </a>
+                                                        ))}
+                                                    </div>
+                                                </div>
+                                            </Popover.Panel>
+                                        </Transition>
+                                    </>
+                                )}
+                            </Popover>
                         </div>
                     </Disclosure.Panel>
                 </>
