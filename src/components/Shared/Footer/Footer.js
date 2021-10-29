ac9c4675b4c7154e7339bfd44568bb520dc51d13
SR TAMIM <saifur.tamim@gmail.com>
2021-10-29 15:44:17 +0600

    loading svg change

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -4,116 +4,116 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <>
-            <footer class="py-8 sm:py-12">
-                <div class="container mx-auto px-4">
-                    <div class="sm:flex sm:flex-wrap sm:-mx-4 md:py-4">
-                        <div class="px-4 sm:w-1/2 md:w-1/4 xl:w-1/6">
-                            <h5 class="text-xl font-bold mb-6">For Travellers</h5>
-                            <ul class="list-none footer-links">
-                                <li class="mb-2">
-                                    <a href="/" class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Why Us</a>
+            <footer className="py-8 sm:py-12">
+                <div className="container mx-auto px-4">
+                    <div className="sm:flex sm:flex-wrap sm:-mx-4 md:py-4">
+                        <div className="px-4 sm:w-1/2 md:w-1/4 xl:w-1/6">
+                            <h5 className="text-xl font-bold mb-6">For Travellers</h5>
+                            <ul className="list-none footer-links">
+                                <li className="mb-2">
+                                    <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Why Us</a>
                                 </li>
-                                <li class="mb-2">
-                                    <a href="/" class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Why Touring</a>
+                                <li className="mb-2">
+                                    <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Why Touring</a>
                                 </li>
-                                <li class="mb-2">
-                                    <a href="/" class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Share Your Review</a>
+                                <li className="mb-2">
+                                    <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Share Your Review</a>
                                 </li>
-                                <li class="mb-2">
-                                    <a href="/" class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Travel Insurance</a>
+                                <li className="mb-2">
+                                    <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Travel Insurance</a>
                                 </li>
-                                <li class="mb-2">
-                                    <a href="/" class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Referral Program</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="px-4 sm:w-1/2 md:w-1/4 xl:w-1/6 mt-8 sm:mt-0">
-                            <h5 class="text-xl font-bold mb-6">Interests</h5>
-                            <ul class="list-none footer-links">
-                                <li class="mb-2">
-                                    <a href="/" class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Adventure</a>
-                                </li>
-                                <li class="mb-2">
-                                    <a href="/" class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Family Holidays</a>
-                                </li>
-                                <li class="mb-2">
-                                    <a href="/" class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Festivals</a>
-                                </li>
-                                <li class="mb-2">
-                                    <a href="/" class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Wildlife</a>
+                                <li className="mb-2">
+                                    <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Referral Program</a>
                                 </li>
                             </ul>
                         </div>
-                        <div class="px-4 sm:w-1/2 md:w-1/4 xl:w-1/6 mt-8 md:mt-0">
-                            <h5 class="text-xl font-bold mb-6">About</h5>
-                            <ul class="list-none footer-links">
-                                <li class="mb-2">
-                                    <a href="/" class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Our Guides</a>
+                        <div className="px-4 sm:w-1/2 md:w-1/4 xl:w-1/6 mt-8 sm:mt-0">
+                            <h5 className="text-xl font-bold mb-6">Interests</h5>
+                            <ul className="list-none footer-links">
+                                <li className="mb-2">
+                                    <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Adventure</a>
                                 </li>
-                                <li class="mb-2">
-                                    <a href="/" class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Locations</a>
+                                <li className="mb-2">
+                                    <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Family Holidays</a>
                                 </li>
-                                <li class="mb-2">
-                                    <a href="/" class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Tour Prices</a>
+                                <li className="mb-2">
+                                    <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Festivals</a>
                                 </li>
-                                <li class="mb-2">
-                                    <a href="/" class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">FAQs</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="px-4 sm:w-1/2 md:w-1/4 xl:w-1/6 mt-8 md:mt-0">
-                            <h5 class="text-xl font-bold mb-6">Help</h5>
-                            <ul class="list-none footer-links">
-                                <li class="mb-2">
-                                    <a href="/" class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Support</a>
-                                </li>
-                                <li class="mb-2">
-                                    <a href="/" class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Help Center</a>
-                                </li>
-                                <li class="mb-2">
-                                    <a href="/" class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Contact Us</a>
+                                <li className="mb-2">
+                                    <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Wildlife</a>
                                 </li>
                             </ul>
                         </div>
-                        <div class="px-4 mt-4 sm:w-1/3 xl:w-1/6 sm:mx-auto xl:mt-0 xl:ml-auto">
+                        <div className="px-4 sm:w-1/2 md:w-1/4 xl:w-1/6 mt-8 md:mt-0">
+                            <h5 className="text-xl font-bold mb-6">About</h5>
+                            <ul className="list-none footer-links">
+                                <li className="mb-2">
+                                    <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Our Guides</a>
+                                </li>
+                                <li className="mb-2">
+                                    <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Locations</a>
+                                </li>
+                                <li className="mb-2">
+                                    <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Tour Prices</a>
+                                </li>
+                                <li className="mb-2">
+                                    <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">FAQs</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="px-4 sm:w-1/2 md:w-1/4 xl:w-1/6 mt-8 md:mt-0">
+                            <h5 className="text-xl font-bold mb-6">Help</h5>
+                            <ul className="list-none footer-links">
+                                <li className="mb-2">
+                                    <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Support</a>
+                                </li>
+                                <li className="mb-2">
+                                    <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Help Center</a>
+                                </li>
+                                <li className="mb-2">
+                                    <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Contact Us</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="px-4 mt-4 sm:w-1/3 xl:w-1/6 sm:mx-auto xl:mt-0 xl:ml-auto">
                             <img src="/logo192.png" className="w-48 mx-auto" alt="" />
                         </div>
                     </div>
 
-                    <div class="sm:flex sm:flex-wrap sm:-mx-4 pt-6 border-t">
-                        <div class="sm:w-full px-4 md:w-1/4">
-                            <h6 class="font-bold mb-2">Stay connected</h6>
-                            <div class="flex">
-                                <a href="/" class="w-10 h-10 rounded-full text-center p-2 hover:bg-green-200">
+                    <div className="sm:flex sm:flex-wrap sm:-mx-4 pt-6 border-t">
+                        <div className="sm:w-full px-4 md:w-1/4">
+                            <h6 className="font-bold mb-2">Stay connected</h6>
+                            <div className="flex">
+                                <a href="/" className="w-10 h-10 rounded-full text-center p-2 hover:bg-green-200">
                                     <img src="/images/facebook.svg" className="w-full h-full" alt="" />
                                 </a>
-                                <a href="/" class="w-10 h-10 rounded-full text-center p-2 hover:bg-green-200">
+                                <a href="/" className="w-10 h-10 rounded-full text-center p-2 hover:bg-green-200">
                                     <img src="/images/twitter.svg" className="w-full h-full" alt="" />
                                 </a>
-                                <a href="/" class="w-10 h-10 rounded-full text-center p-2 hover:bg-green-200">
+                                <a href="/" className="w-10 h-10 rounded-full text-center p-2 hover:bg-green-200">
                                     <img src="/images/linkedin-in.svg" className="w-full h-full" alt="" />
                                 </a>
                             </div>
                         </div>
-                        <div class="px-4 sm:w-1/2 md:w-1/4 mt-4 md:mt-0">
-                            <h6 class="font-bold mb-2">Address</h6>
-                            <address class="not-italic mb-4 text-sm">
+                        <div className="px-4 sm:w-1/2 md:w-1/4 mt-4 md:mt-0">
+                            <h6 className="font-bold mb-2">Address</h6>
+                            <address className="not-italic mb-4 text-sm">
                                 123 6th St.<br />
                                 Melbourne, FL 32904
                             </address>
                         </div>
-                        <div class="px-4 sm:w-1/2 md:w-1/4 mt-4 md:mt-0">
-                            <h6 class="font-bold mb-2">For Operators</h6>
-                            <a href="/" class="mb-4 text-sm">
+                        <div className="px-4 sm:w-1/2 md:w-1/4 mt-4 md:mt-0">
+                            <h6 className="font-bold mb-2">For Operators</h6>
+                            <a href="/" className="mb-4 text-sm">
                                 Admin Page
                             </a>
                             <br />
-                            <a href="/" class="mb-4 text-sm">
+                            <a href="/" className="mb-4 text-sm">
                                 Moderator Page
                             </a>
                         </div>
                         <div className="px-4 sm:w-1/2 md:w-1/4 mt-4 md:mt-0">
-                            <h6 class="font-bold mb-2">Copyright</h6>
+                            <h6 className="font-bold mb-2">Copyright</h6>
                             <p className="text-sm"
                             >2021 &copy; All rights reserved <br />
                                 by <a href="https://twitter.com/TaMiM__tamim"
--- src/components/Shared/Loading/Loading.js
+++ src/components/Shared/Loading/Loading.js
@@ -4,8 +4,8 @@ const Loading = () => {
     return (
         <section className="flex justify-center">
             <div
-                class="text-4xl text-black rounded-xl font-extrabold my-36" disabled>
-                <img class="animate-spin h-28 w-28 inline-block mr-3" src="/images/spinner.svg" alt="spinner" />
+                className="text-4xl text-black rounded-xl font-extrabold my-36" disabled>
+                <img className="h-28 w-28 inline-block mr-3" src="/images/spinner.svg" alt="spinner" />
             </div>
         </section>
     );
