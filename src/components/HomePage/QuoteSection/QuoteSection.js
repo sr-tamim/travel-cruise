6e08ef23f9e5f79a1e4fdff0b33cc5ddf85d7c08
SR TAMIM <saifur.tamim@gmail.com>
2021-10-31 22:48:21 +0600

    little ui edit

--- src/components/HomePage/QuoteSection/QuoteSection.js
+++ src/components/HomePage/QuoteSection/QuoteSection.js
@@ -13,10 +13,10 @@ const QuoteSection = () => {
         <section className="pb-0 px-0">
             <div className="quote-wrap py-36 text-white text-center"
                 style={quoteStyles}>
-                <blockquote className="text-xl lg:text-2xl font-bold my-10 sm:px-24 lg:px-72">
+                <q className="block text-xl lg:text-2xl font-medium my-10 sm:px-24 lg:px-72">
                     The real voyage of discovery consists not in seeking new landscapes, but in having new eyes.
-                </blockquote>
-                <h4 className="text-xl font-bold my-10">ANDREA STEINBECK</h4>
+                </q>
+                <h4 className="text-2xl lg:text-3xl font-bold my-10">ANDREA STEINBECK</h4>
             </div>
         </section>
     );
